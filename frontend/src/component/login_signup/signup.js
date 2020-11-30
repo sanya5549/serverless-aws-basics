f8da414d83d9b9f717521083ba9f56698046f9de
Sanchit <rajmbcoderx@gmail.com>
2020-12-01 02:24:00 +0530

    signup and verification added

--- frontend/src/component/login_signup/signup.js
+++ frontend/src/component/login_signup/signup.js
@@ -1,81 +1,143 @@
-import React from 'react';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { useHistory } from "react-router-dom";
+import LoaderButton from "../../preloader/LoaderButton";
+import { useAppContext } from "../../libs/contextLib";
+import { useFormFields } from  "../../libs/hooksLib";
+import "./signup.css";
+import { Auth } from "aws-amplify";
 
-import { Card, Button, Form } from 'react-bootstrap';
-import './login.css';
-import userImage from '../../assets/user.png';
-import axios from 'axios'
+export default function Signup() {
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    confirmationCode: "",
+  });
+  const history = useHistory();
+  const [newUser, setNewUser] = useState(null);
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
 
+  function validateForm() {
+    return (
+      fields.email.length > 0 &&
+      fields.password.length > 0 &&
+      fields.password === fields.confirmPassword
+    );
+  }
 
-class SignupComponent extends React.Component{
+  function validateConfirmationForm() {
+    return fields.confirmationCode.length > 0;
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            email:'',
-            password:'',
-            error:{}
-        };
-
-
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+  async function handleSubmit(event) {
+    event.preventDefault();
+  
+    setIsLoading(true);
+  
+    try {
+      const newUser = await Auth.signUp({
+        username: fields.email,
+        password: fields.password,
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (e) {
+    //   onError(e);
+      setIsLoading(false);
     }
-
-    onChange(e) {
-        this.setState({ [e.target.name]: e.target.value })
+  }
+  
+  async function handleConfirmationSubmit(event) {
+    event.preventDefault();
+  
+    setIsLoading(true);
+  
+    try {
+      await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+      await Auth.signIn(fields.email, fields.password);
+  
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+    //   onError(e);
+      setIsLoading(false);
     }
+  }
 
+  function renderConfirmationForm() {
+    return (
+      <Form onSubmit={handleConfirmationSubmit}>
+        <Form.Group controlId="confirmationCode" size="lg">
+          <Form.Label>Confirmation Code</Form.Label>
+          <Form.Control
+            autoFocus
+            type="tel"
+            onChange={handleFieldChange}
+            value={fields.confirmationCode}
+          />
+          <Form.Text muted>Please check your email for the code.</Form.Text>
+        </Form.Group>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          variant="success"
+          isLoading={isLoading}
+          disabled={!validateConfirmationForm()}
+        >
+          Verify
+        </LoaderButton>
+      </Form>
+    );
+  }
 
-    onSubmit(e) {
-        e.preventDefault()
-    
-        const user = {
-          email: this.state.email,
-          password: this.state.password
-        }
-    
-        axios.put("http://localhost:5000/signup", user).then((resp) => {
-            if (resp["result"] === "SUCCESS"){
-                alert("Sign Up Successfull");
-            }else{
-                let err = resp["error"];
-                this.setState({error : {err}});
-            }
-        })
-      }
-    render(){
-        return(
-            <div className="loginDiv">
-                <Card className="loginCard" style={{  }}>
-                <Card.Img className="userImg" variant="top" src={userImage} />
-                <Card.Body>
-                    <Card.Title style={{fontSize:"30px", textAlign:"center"}}>Sign Up</Card.Title>
-                    <Form onSubmit={this.onSubmit}>
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control name="email" type="email" placeholder="Enter email" onChange={this.onChange}/>
-                            <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
+  function renderForm() {
+    return (
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="email" size="lg">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="password" size="lg">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={fields.password}
+            onChange={handleFieldChange}
+          />
+        </Form.Group>
+        <Form.Group controlId="confirmPassword" size="lg">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            onChange={handleFieldChange}
+            value={fields.confirmPassword}
+          />
+        </Form.Group>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          variant="success"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
+          Signup
+        </LoaderButton>
+      </Form>
+    );
+  }
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control name="password" type="password" placeholder="Password"  onChange={this.onChange} />
-                        </Form.Group>
-                        {/* <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group> */}
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Card.Body>
-                </Card>
-            </div>
-            
-        );
-    }
+  return (
+    <div className="Signup">
+      {newUser === null ? renderForm() : renderConfirmationForm()}
+    </div>
+  );
 }
-
-export default SignupComponent;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./component/home/home";
 import Login from "./component/login_signup/login";
+import Signup from "./component/login_signup/signup";
 
 export default function Routes() {
   return (
@@ -12,6 +13,9 @@ export default function Routes() {
       <Route exact path="/login">
         <Login />
       </Route>
+      <Route exact path="/signup">
+        <Signup />
+    </Route>
       {/* Finally, catch all unmatched routes */}
     </Switch>
   );
